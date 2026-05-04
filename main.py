4ce4a1d5033df115f20ceebca68b07bf9aca63be
ElectricalOutlet <alexplaysgamezzz@outlook.com>
2026-05-04 15:55:51 -0700

    fix issue where highscore doesn't save after exiting (I think)

--- main.py
+++ main.py
@@ -6,6 +6,12 @@ from scripts.space import Vector2
 from scripts.obstacle import ObstacleSpawner
 from scripts.player import Player
 
+def save(highscore, spike_total):
+    with open('./data/save.txt', 'w') as f:
+        if spike_total > highscore:
+            highscore = spike_total
+            f.write(f'hs:{spike_total}')
+
 def main():
     pg.init()
 
@@ -59,6 +65,7 @@ def main():
             input_state.update_input(event)
 
         if input_state.events['quit'].just_pressed:
+            save(highscore, spike_spawner.total_spawned)
             pg.quit()
             sys.exit()
 
@@ -77,10 +84,7 @@ def main():
 
             if not player.alive:
                 game_state = 'death'
-                with open('./data/save.txt', 'w') as f:
-                    if spike_spawner.total_spawned > highscore:
-                        highscore = spike_spawner.total_spawned
-                        f.write(f'hs:{spike_spawner.total_spawned}')
+                save(highscore, spike_spawner.total_spawned)
                 pg.mouse.set_visible(True)
                 pg.mixer.music.pause()
 
